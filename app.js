d85b09eee194c2c8edc69379a21022074d7dc47d
Benjamin Boberg <benjamin.boberg8318@stu.mtec.edu>
2022-10-13 20:42:21 -0600

    week5day4

---
+++ app.js
@@ -0,0 +1,5 @@
+$('.navItem').each('color','white');
+$('#nav').css('background-color','#145');
+$('li:contains("An even list item")').css('background-color', '#ccc');
+$('li:contains("An odd list item")').css('background-color', '#aaa');
+$('input[placeholder*= "First Name"]').html('Ben');
